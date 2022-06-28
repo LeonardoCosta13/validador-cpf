ca6e18b90daf0e5082e2c6e88f4a2a5cfdeefee6
leonardocosta <leocostasousa123@gmail.com>
2022-06-28 09:38:21 -0300

    Validação do primeirop digito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,27 @@ export class AppComponent {
 
 
   validar(valor: any){
+  
+
+    let cpf: any = valor;
+    if(cpf.length == 11){
+      let soma = 0;
+      soma += cpf[0] * 10;
+      soma += cpf[1] * 9;
+      soma += cpf[3] * 8;
+      soma += cpf[4] * 7;
+      soma += cpf[5] * 6;
+      soma += cpf[6] * 5;
+      soma += cpf[7] * 4;
+      soma += cpf[8] * 3;
+      soma += cpf[9] * 2;
+     soma = (soma * 10)%11;
+    console.log(soma);
+    }
+
+    //let cpf: any = "24805516011";
+
    
-    console.log(this.cpf);
-    this.cpf = valor;
-    console.log(this.cpf);
   
   }
 }
